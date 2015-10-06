bb0320a01033ebb47f19142be74767e375b8733a
chenchc <bryan830401@gmail.com>
2015-10-06 15:17:01 +0800

    Naive approach: ~1400

--- Fib2584Ai.cpp
+++ Fib2584Ai.cpp
@@ -1,4 +1,7 @@
 #include "Fib2584Ai.h"
+#include <cstring>
+#include <algorithm>
+#include <iostream>
 
 Fib2584Ai::Fib2584Ai()
 {
@@ -7,14 +10,16 @@ Fib2584Ai::Fib2584Ai()
 void Fib2584Ai::initialize(int argc, char* argv[])
 {
 	srand(time(NULL));
+	greedy.initialize();
 	return;
 }
 
 
 MoveDirection Fib2584Ai::generateMove(int board[4][4])
 {
-	MoveDirection randomMove = static_cast<MoveDirection>(rand() % 4);
-	return randomMove;
+	//MoveDirection randomMove = static_cast<MoveDirection>(rand() % 4);
+	//return randomMove;
+	return greedy(board);
 }
 
 void Fib2584Ai::gameOver(int board[4][4], int iScore)
@@ -22,7 +27,131 @@ void Fib2584Ai::gameOver(int board[4][4], int iScore)
 	return;
 }
 
-/**********************************
-You can implement any additional functions
-you may need.
-**********************************/
+Fib2584Ai::Greedy::Greedy()
+{}
+
+void Fib2584Ai::Greedy::initialize()
+{
+}
+
+MoveDirection Fib2584Ai::Greedy::operator()(int board[4][4])
+{
+	buildTileQueue(board);
+	buildInvBoard(board);
+	for (;;) {
+		if (tileQueue.size() == 0) {
+			return static_cast<MoveDirection>(
+				rand() % 2 ? MOVE_RIGHT : MOVE_DOWN);
+		}
+		Cor largest = tileQueue.back();
+		tileQueue.pop_back();
+		if (largest.num == 0) {
+			return static_cast<MoveDirection>(
+				rand() % 2 ? MOVE_RIGHT : MOVE_DOWN);
+		}
+		if (canMoveLeft(largest.row, largest.col)) {
+			//std::cout << largest.row << " " << largest.col << " left" << std::endl;
+			return MOVE_LEFT;
+		}
+		if (canMoveUp(largest.row, largest.col)) {
+			//std::cout << largest.row << " " << largest.col << " up" << std::endl;
+			return MOVE_UP;
+		}
+	}	
+}
+
+void Fib2584Ai::Greedy::buildTileQueue(int board[4][4])
+{
+	tileQueue.clear();
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			Cor cor;
+
+			cor.row = i;
+			cor.col = j;
+			cor.num = board[i][j];
+			tileQueue.push_back(cor);
+		}
+	}
+	std::sort(tileQueue.begin(), tileQueue.end());
+}
+
+void Fib2584Ai::Greedy::buildInvBoard(int board[4][4])
+{
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			int k;
+
+			for (k = 0; k < 32; k++) {
+				if (board[i][j] == fibonacci[k])
+					break;
+			}
+			invBoard[i][j] = k;
+		}
+	}
+}
+
+inline bool invFibNeighbor(int a, int b)
+{
+	if (a == 1 && b == 1)
+		return true;
+	else if (a - b == 1)
+		return true;
+	else
+		return b - a == 1;
+}
+
+bool Fib2584Ai::Greedy::canMoveLeft(int row, int col) const
+{
+	int i;
+
+	// Look leftward
+	for (i = col - 1; i >= 0; i--) {
+		if (invBoard[row][i] == 0)
+			continue;
+		if (invFibNeighbor(invBoard[row][col], invBoard[row][i]))
+			return true;
+		break;
+	}
+	if (col != 0 && i == -1)
+		return true;
+
+	// Look rightward
+	for (i = col + 1; i < 4; i++) {
+		if (invBoard[row][i] == 0)
+			continue;
+		if (invFibNeighbor(invBoard[row][col], invBoard[row][i]))
+			return true;
+		break;
+	}
+
+	return false;
+}
+
+bool Fib2584Ai::Greedy::canMoveUp(int row, int col) const
+{
+	int i;
+
+	// Look upward
+	for (i = row - 1; i >= 0; i--) {
+		if (invBoard[i][col] == 0)
+			continue;
+		if (invFibNeighbor(invBoard[row][col], invBoard[i][col]))
+			return true;
+		break;
+	}
+	if (row != 0 && i == -1)
+		return true;
+
+	// Look rightward
+	for (i = row + 1; i < 4; i++) {
+		if (invBoard[i][col] == 0)
+			continue;
+		if (invFibNeighbor(invBoard[row][col], invBoard[i][col]))
+			return true;
+		break;
+	}
+
+	return false;
+}
+
--- Fib2584Ai.h
+++ Fib2584Ai.h
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 #include "Fib2584/MoveDirection.h"
 
 class Fib2584Ai
@@ -16,10 +17,44 @@ public:
 	// do some action when game over
 	void gameOver(int board[4][4], int iScore);
 
-	/**********************************
-	You can implement any additional functions
-	or define any variables you may need.
-	**********************************/
+private:
+	class Greedy
+	{
+	public:
+		Greedy();
+		void initialize();
+		MoveDirection operator()(int board[4][4]);
+	private:
+		struct Cor {
+			int row, col;
+			int num;
+
+			bool operator<(const Cor &rhs) const
+			{
+				return num < rhs.num;
+			}
+		};
+
+		const int positionWeight[4][4] = 
+			{{4, 3, 2, 1},
+			 {3, 0, 0, 0},
+			 {2, 0, 0, 0},
+			 {1, 0, 0, 0}};
+		const int fibonacci[32]	= {0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 
+			233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 
+			46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 
+			2178309};
+
+		int invBoard[4][4];
+		std::vector<Cor> tileQueue;
+
+		void buildTileQueue(int board[4][4]);
+		void buildInvBoard(int board[4][4]);
+		bool canMoveLeft(int row, int col) const;
+		bool canMoveUp(int row, int col) const;
+	};
+	
+	Greedy greedy;
 };
 
 #endif
